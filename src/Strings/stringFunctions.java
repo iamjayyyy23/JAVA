5bb3b08ef46346291b24a2ed9336fdd03b3eb65f
jayanta23 <mrsarkar1923@gmail.com>
2026-04-30 11:47:33 +0530

    String Functions at tuition.

---
+++ src/Strings/stringFunctions.java
@@ -0,0 +1,40 @@
+package Strings;
+
+public class stringFunctions {
+    public static void main(){
+//        String s = "Jayanta Sarkar";
+//        System.out.print(s);
+
+//        String s = new String("Jayanta Sarkar");
+//        System.out.print(s);
+
+//        String s = "jayanta sarkar";
+//        System.out.print(s.toUpperCase());
+
+//        String s = "Jayanta Sarkar";
+//        System.out.print(s.toLowerCase());
+
+//        String s = "Jayanta";
+//        System.out.print(s.concat("Sarkar"));
+
+//        String s = "Jayanta";
+//        System.out.print(s.equals("Jayanta"));
+
+//        String s = "Jayanta";
+//        System.out.print(s.charAt(4));
+
+//        String s = "Jayanta";
+//        System.out.print(s.length());
+
+//        String s = "Jayanta";
+//        System.out.print(s.contains("J"));
+
+//        String s = "Jayanta Sarkar";
+//        System.out.print(s.substring(0,7));
+
+//        StringBuilder sb = new StringBuilder("Jayanta");
+//        String ln = " Sarkar";
+//        System.out.print(sb.append(ln));
+    }
+
+}
